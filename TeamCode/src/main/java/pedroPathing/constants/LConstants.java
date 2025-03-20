5d6df2880c7951db10dccac8dafed179ebad808c
littlefonz <littlejfonz@outlook.com>
2025-03-20 14:47:03 -0400

    Used RR values

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -3,16 +3,16 @@ package pedroPathing.constants;
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
 
-public class LConstants {
+public class LConstants { // hi james
     static {
-        ThreeWheelConstants.forwardTicksToInches = .0008;
-        ThreeWheelConstants.strafeTicksToInches = .0007;
-        ThreeWheelConstants.turnTicksToInches = .0003;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "fl";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "br";
+        ThreeWheelConstants.forwardTicksToInches = 100.0/134466.0;
+        ThreeWheelConstants.strafeTicksToInches = 100.0/134466.0;
+        ThreeWheelConstants.turnTicksToInches = 100.0/134466.0;
+        ThreeWheelConstants.leftY = -2.618;
+        ThreeWheelConstants.rightY = 2.618;
+        ThreeWheelConstants.strafeX = 2.618;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "br";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "fl";
         ThreeWheelConstants.strafeEncoder_HardwareMapName = "fr";
         ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.rightEncoderDirection = Encoder.FORWARD;
